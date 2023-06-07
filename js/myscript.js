98dd16fce7b2f4dfd401d7ec55516f192460e30d
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-07 17:27:40 +0200

    set array obj prop.visible to false except first

--- js/myscript.js
+++ js/myscript.js
@@ -29,7 +29,7 @@ createApp ({
                 {
                     name: 'Fabio',
                     avatar: './img/avatar_2.jpg',
-                    visible: true,
+                    visible: false,
                     messages: [
                         {
                             date: '20/03/2020 16:30:00',
@@ -51,7 +51,7 @@ createApp ({
                 {
                     name: 'Samuele',
                     avatar: './img/avatar_3.jpg',
-                    visible: true,
+                    visible: false,
                     messages: [
                         {
                             date: '28/03/2020 10:10:40',
@@ -73,7 +73,7 @@ createApp ({
                 {
                     name: 'Alessandro B.',
                     avatar: './img/avatar_4.jpg',
-                    visible: true,
+                    visible: false,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
@@ -90,7 +90,7 @@ createApp ({
                 {
                     name: 'Alessandro L.',
                     avatar: './img/avatar_5.jpg',
-                    visible: true,
+                    visible: false,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
@@ -107,7 +107,7 @@ createApp ({
                 {
                     name: 'Claudia',
                     avatar: './img/avatar_6.jpg',
-                    visible: true,
+                    visible: false,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
@@ -129,7 +129,7 @@ createApp ({
                 {
                     name: 'Federico',
                     avatar: './img/avatar_7.jpg',
-                    visible: true,
+                    visible: false,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
@@ -146,7 +146,7 @@ createApp ({
                 {
                     name: 'Davide',
                     avatar: './img/avatar_8.jpg',
-                    visible: true,
+                    visible: false,
                     messages: [
                         {
                             date: '10/01/2020 15:30:55',
@@ -169,6 +169,6 @@ createApp ({
         }
     },
     methods: {
-        
+
     }
 }) .mount('#app');
